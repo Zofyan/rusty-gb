0e0213ecbc1adbccac2a85307dbccd152ce2efa5
Sofyan <sofyan_aarrass@icloud.com>
2024-05-02 01:35:47 +0200

    jumps done

--- src/bus.rs
+++ src/bus.rs
@@ -1,34 +1,34 @@
-const ROM_0: u16 = 0x0000;
-const ROM_0_END: u16 = 0x3FFF;
-const ROM_N: u16 = 0x4000;
-const ROM_N_END: u16 = 0x7FFF;
-const VRAM: u16 = 0x8000;
-const VRAM_END: u16 = 0x9FFF;
-const ERAM: u16 = 0xA000;
-const ERAM_END: u16 = 0xBFFF;
-const WRAM_0: u16 = 0xC000;
-const WRAM_0_END: u16 = 0xCFFF;
-const WRAM_N: u16 = 0xD000;
-const WRAM_N_END: u16 = 0xDFFF;
-const OAM: u16 = 0xFE00;
-const OAM_END: u16 = 0xFE9F;
-const IO_REGISTERS: u16 = 0xFF00;
-const IO_REGISTERS_END: u16 = 0xFF7F;
-const HRAM: u16 = 0xFF80;
-const HRAM_END: u16 = 0xFFFE;
-const INT_ENABLE: u16 = 0xFFFF;
-const INT_ENABLE_END: u16 = 0xFFFF;
-const INT_REQUEST: u16 = 0xFF0F;
+pub const ROM_0: u16 = 0x0000;
+pub const ROM_0_END: u16 = 0x3FFF;
+pub const ROM_N: u16 = 0x4000;
+pub const ROM_N_END: u16 = 0x7FFF;
+pub const VRAM: u16 = 0x8000;
+pub const VRAM_END: u16 = 0x9FFF;
+pub const ERAM: u16 = 0xA000;
+pub const ERAM_END: u16 = 0xBFFF;
+pub const WRAM_0: u16 = 0xC000;
+pub const WRAM_0_END: u16 = 0xCFFF;
+pub const WRAM_N: u16 = 0xD000;
+pub const WRAM_N_END: u16 = 0xDFFF;
+pub const OAM: u16 = 0xFE00;
+pub const OAM_END: u16 = 0xFE9F;
+pub const IO_REGISTERS: u16 = 0xFF00;
+pub const IO_REGISTERS_END: u16 = 0xFF7F;
+pub const HRAM: u16 = 0xFF80;
+pub const HRAM_END: u16 = 0xFFFE;
+pub const INT_ENABLE: u16 = 0xFFFF;
+pub const INT_ENABLE_END: u16 = 0xFFFF;
+pub const INT_REQUEST: u16 = 0xFF0F;
 
-const ROM_0_SIZE: u16 = ROM_0_END - ROM_0;
-const ROM_N_SIZE: u16 = ROM_N_END - ROM_N;
-const VRAM_SIZE: u16 = VRAM_END - VRAM;
-const ERAM_SIZE: u16 = ERAM_END - ERAM;
-const WRAM_0_SIZE: u16 = WRAM_0_END - WRAM_0;
-const WRAM_N_SIZE: u16 = WRAM_N_END - WRAM_N;
-const OAM_SIZE: u16 = OAM_END - OAM;
-const IO_REGISTERS_SIZE: u16 = IO_REGISTERS_END - IO_REGISTERS;
-const HRAM_SIZE: u16 = HRAM_END - HRAM;
+pub const ROM_0_SIZE: u16 = ROM_0_END - ROM_0;
+pub const ROM_N_SIZE: u16 = ROM_N_END - ROM_N;
+pub const VRAM_SIZE: u16 = VRAM_END - VRAM;
+pub const ERAM_SIZE: u16 = ERAM_END - ERAM;
+pub const WRAM_0_SIZE: u16 = WRAM_0_END - WRAM_0;
+pub const WRAM_N_SIZE: u16 = WRAM_N_END - WRAM_N;
+pub const OAM_SIZE: u16 = OAM_END - OAM;
+pub const IO_REGISTERS_SIZE: u16 = IO_REGISTERS_END - IO_REGISTERS;
+pub const HRAM_SIZE: u16 = HRAM_END - HRAM;
 
 pub struct Bus {
     rom_0: [u8; ROM_0_SIZE as usize],
@@ -46,26 +46,54 @@ impl Bus {
     pub fn new() -> Bus {
         Bus { rom_0: [0; ROM_0_SIZE as usize], rom_n: [0; ROM_N_SIZE as usize], vram: [0; VRAM_SIZE as usize], eram: [0; ERAM_SIZE as usize], wram_0: [0;  WRAM_0_SIZE as usize], wram_n: [0; WRAM_N_SIZE as usize], oam: [0; OAM_SIZE as usize], io_registers: [0; IO_REGISTERS_SIZE as usize], hram: [0; HRAM_SIZE as usize] }
     }
-    
-    pub fn get(&self, address: u16) -> u8 {
-        match address { 
-            ..=ROM_0_END => self.rom_0[(address - ROM_0) as usize],
-            ROM_N..=ROM_N_END => self.rom_n[(address - ROM_N) as usize],
-            VRAM..=VRAM_END => self.vram[(address - VRAM) as usize],
-            ERAM..=ERAM_END => self.eram[(address - ERAM) as usize],
-            WRAM_0..=WRAM_0_END => self.wram_0[(address - WRAM_0) as usize],
-            _ => panic!("Not implemented yet!")
-        }
-    }
-    pub fn set(&mut self, address: u16, value: u8) {
-        let target = match address {
+    fn get_target_mut(&mut self, address: u16) -> &mut u8{
+        match address {
             ..=ROM_0_END => &mut self.rom_0[(address - ROM_0) as usize],
             ROM_N..=ROM_N_END => &mut self.rom_n[(address - ROM_N) as usize],
             VRAM..=VRAM_END => &mut self.vram[(address - VRAM) as usize],
             ERAM..=ERAM_END => &mut self.eram[(address - ERAM) as usize],
             WRAM_0..=WRAM_0_END => &mut self.wram_0[(address - WRAM_0) as usize],
+            WRAM_N..=WRAM_N_END => &mut self.wram_n[(address - WRAM_N) as usize],
+            OAM..=OAM_END => &mut self.oam[(address - OAM) as usize],
+            IO_REGISTERS..=IO_REGISTERS_END => &mut self.io_registers[(address - IO_REGISTERS) as usize],
+            HRAM..=HRAM_END => &mut self.hram[(address - HRAM) as usize],
             _ => panic!("Not implemented yet!")
-        };
+        }
+    }
+    fn get_target(&self, address: u16) -> &u8{
+        match address {
+            ..=ROM_0_END => &self.rom_0[(address - ROM_0) as usize],
+            ROM_N..=ROM_N_END => &self.rom_n[(address - ROM_N) as usize],
+            VRAM..=VRAM_END => &self.vram[(address - VRAM) as usize],
+            ERAM..=ERAM_END => &self.eram[(address - ERAM) as usize],
+            WRAM_0..=WRAM_0_END => &self.wram_0[(address - WRAM_0) as usize],
+            WRAM_N..=WRAM_N_END => &self.wram_n[(address - WRAM_N) as usize],
+            OAM..=OAM_END => &self.oam[(address - OAM) as usize],
+            IO_REGISTERS..=IO_REGISTERS_END => &self.io_registers[(address - IO_REGISTERS) as usize],
+            HRAM..=HRAM_END => &self.hram[(address - HRAM) as usize],
+            _ => panic!("Not implemented yet!")
+        }
+    }
+    pub fn get(&self, address: u16) -> u8 {
+        *self.get_target(address)
+    }
+    pub fn gets(&self, address: u16) -> i8 {
+        *self.get_target(address) as i8
+    }
+    pub fn get16(&self, address: u16) -> u16 {
+        let (target1, target2) = (self.get_target(address), self.get_target(address + 1));
+        let v1 = *target1 as u16;
+        let v2 = *target2 as u16;
+        v2 << 8 | v1
+    }
+    pub fn set(&mut self, address: u16, value: u8) {
+        let target = self.get_target_mut(address);
         *target = value
+    }
+    pub fn set16(&mut self, address: u16, value: u16) {
+        let target = self.get_target_mut(address);
+        *target = value as u8;
+        let target = self.get_target_mut(address + 1);
+        *target = (value >> 8) as u8
     }
 }
--- src/cpu.rs
+++ src/cpu.rs
@@ -15,16 +15,66 @@ impl Cpu {
     }
 
     pub fn step(&mut self) {
-        if  !self.load(0, 0) &&
-            !self.alu(0, 0) {
+        if !self.load((0, 0)) &&
+            !self.alu((0, 0)) {
             panic!("Illegal instruction")
         }
     }
 
     fn _cycles(&self, count: usize) {}
+    fn _pc(&mut self, count: u16) { self.registers.set_pc(self.registers.get_pc() + count) }
 
-    fn load(&mut self, inst1: u8, inst2: u8) -> bool{
-        let val: u8 = match (inst1, inst2) {
+    fn pop(&mut self) -> u16 {
+        self.registers.inc16_sp();
+        self.registers.inc16_sp();
+        let val = self.bus.get16(self.registers.get_sp() - 2);
+
+        self._cycles(3);
+        self._pc(1);
+        val
+    }
+    fn push(&mut self, value: u16) {
+        self.bus.set16(self.registers.get_sp() - 2, value);
+        self.registers.dec16_sp();
+        self.registers.dec16_sp();
+
+        self._cycles(4);
+        self._pc(1);
+    }
+    fn load16(&mut self, inst: (u8, u8)) -> bool {
+        let val: u16 = match inst {
+            (0..=3, 1) => self.bus.get16(self.registers.get_sp() + 1),
+            (0, 8) => self.registers.get_sp(),
+            (0xf, 9) => self.registers.get_hl(),
+            _ => return false
+        };
+        match inst {
+            (0, 1) => self.registers.set_bc(val),
+            (1, 1) => self.registers.set_de(val),
+            (2, 1) => self.registers.set_hl(val),
+            (3, 1) => self.registers.set_sp(val),
+            (0, 8) => self.bus.set16(self.bus.get16(self.registers.get_pc() + 1), val),
+            (0xf, 9) => self.registers.set_sp(val),
+            _ => panic!("Illegal instruction")
+        }
+        match inst {
+            (0, 8) => {
+                self._cycles(5);
+                self._pc(3)
+            }
+            (0xf, 9) => {
+                self._cycles(2);
+                self._pc(1)
+            }
+            _ => {
+                self._cycles(3);
+                self._pc(3)
+            }
+        }
+        true
+    }
+    fn load(&mut self, inst: (u8, u8)) -> bool {
+        let val: u8 = match inst {
             (4..=7, 0x0 | 0x8) => self.registers.get_b(),
             (4..=7, 0x1 | 0x9) => self.registers.get_c(),
             (4..=7, 0x2 | 0xa) => self.registers.get_d(),
@@ -38,7 +88,7 @@ impl Cpu {
             (0..=3, 0x6 | 0xe) => self.bus.get(self.registers.get_pc() + 1),
             _ => return false
         };
-        match (inst1, inst2) {
+        match inst {
             (0x4, ..=7) | (0, 0x6) => self.registers.set_b(val),
             (0x4, 8..) | (0, 0xe) => self.registers.set_c(val),
             (0x5, ..=7) | (1, 0x6) => self.registers.set_d(val),
@@ -51,23 +101,41 @@ impl Cpu {
             (0x1, 0x2) => self.bus.set(self.registers.get_de(), val),
             _ => return false
         };
-        match (inst1, inst2) {
+        match inst {
             (2, 2 | 0xa) => self.registers.set_hl(self.registers.get_hl() + 1),
             (3, 2 | 0xa) => self.registers.set_hl(self.registers.get_hl() - 1),
             _ => ()
         }
+        match inst {
+            (0..=3, 2 | 0xa) | (4..=7, 6 | 0xe) | (7, 0..=7) => {
+                self._cycles(2);
+                self._pc(1)
+            }
+            (0..=2, 6) | (0..=3, 0xe) => {
+                self._cycles(2);
+                self._pc(2)
+            }
+            (3, 6) => {
+                self._cycles(3);
+                self._pc(2)
+            }
+            _ => {
+                self._cycles(1);
+                self._pc(1)
+            }
+        }
         true
     }
 
-    pub fn alu16(&mut self, inst1: u8, inst2: u8) -> bool{
-        let val = match (inst1, inst2) {
+    fn alu16(&mut self, inst: (u8, u8)) -> bool {
+        let val = match inst {
             (0, 9) => self.registers.get_bc(),
             (1, 9) => self.registers.get_de(),
             (2, 9) => self.registers.get_hl(),
             (3, 9) => self.registers.get_sp(),
             _ => 0
         };
-        match (inst1, inst2) {
+        match inst {
             (0, 3) => self.registers.inc16_bc(),
             (1, 3) => self.registers.inc16_de(),
             (2, 3) => self.registers.inc16_hl(),
@@ -79,14 +147,16 @@ impl Cpu {
             (0..=3, 9) => self.registers.add16(val),
             _ => ()
         }
-        match (inst1, inst2) {
+        self._cycles(2);
+        self._pc(1);
+        match inst {
             (0..=3, 3 | 9 | 0xb) => true,
             _ => false
         }
     }
-    pub fn alu(&mut self, inst1: u8, inst2: u8) -> bool{
+    fn alu(&mut self, inst: (u8, u8)) -> bool {
         let old_carry_flag = self.registers.get_flag_c();
-        let val = match (inst1, inst2) {
+        let val = match inst {
             (8..=0xb, 0 | 0x8) => self.registers.get_b(),
             (8..=0xb, 1 | 0x9) => self.registers.get_c(),
             (8..=0xb, 2 | 0xa) => self.registers.get_d(),
@@ -114,7 +184,7 @@ impl Cpu {
             (0xc..=0xf, 6 | 0xe) => self.bus.get(self.registers.get_pc() + 1),
             _ => return false
         };
-        match (inst1, inst2) {
+        match inst {
             (0x8, 0..=0x7) | (0xc, 6) => self.registers.add(val),
             (0x8, 8..=0xf) | (0xc, 0xe) => self.registers.addc(val),
             (0x9, 0..=0x7) | (0xd, 6) => self.registers.sub(val),
@@ -133,8 +203,49 @@ impl Cpu {
             (3, 0xc | 0xd) => self.registers.set_a(val),
             _ => return false
         }
-        if inst1 <= 3 {
+        if inst.0 <= 3 {
             self.registers.set_flag_c(old_carry_flag);
+        }
+        match inst {
+            (0xc..=0xf, 6 | 0xe) => {
+                self._cycles(2);
+                self._pc(2)
+            }
+            (8..=0xb, 6 | 0xe) => {
+                self._cycles(2);
+                self._pc(1)
+            }
+            _ => {
+                self._cycles(1);
+                self._pc(1)
+            }
+        }
+        true
+    }
+    fn jump(&mut self, inst: (u8, u8)) -> bool {
+        let condition = match inst {
+            (2, 0) | (0xc, 2) => !self.registers.get_flag_z(),
+            (2, 8) | (0xc, 0xa) => self.registers.get_flag_z(),
+            (3, 0) | (0xd, 2) => !self.registers.get_flag_c(),
+            (3, 8) | (0xd, 0xa) => self.registers.get_flag_c(),
+            _ => true
+        };
+        let pc = self.registers.get_pc();
+        if condition {
+            match inst {
+                (2..=3, 0) | (1..=3, 8) => self.registers.set_pc(pc.wrapping_add_signed(self.bus.gets(pc + 1) as i16) + 2),
+                (0xc..=0xd, 2) | (0xc..=0xd, 0xa) | (0xc, 0x3) => self.registers.set_pc(self.bus.get16(pc + 1)),
+                _ => return false
+            }
+            self._cycles(3)
+        } else{
+            if inst.0 <= 3 {
+                self._pc(2)
+            } else{
+                self._pc(3);
+                self._cycles(1)
+            }
+            self._cycles(2)
         }
         true
     }
@@ -159,26 +270,26 @@ mod tests {
         cpu.registers.set_c(x1);
         cpu.registers.set_d(x2);
 
-        cpu.load(0x5, 0x1);
+        cpu.load((0x5, 0x1));
         assert_eq!(cpu.registers.get_c(), x1);
         assert_eq!(cpu.registers.get_d(), x1);
 
         cpu.registers.set_hl(x5);
         cpu.registers.set_a(x3);
 
-        cpu.load(0x2, 0x2);
+        cpu.load((0x2, 0x2));
         assert_eq!(cpu.bus.get(x5), x3);
 
         cpu.registers.set_b(x4);
-        cpu.load(0x7, 0x0);
+        cpu.load((0x7, 0x0));
         assert_eq!(cpu.bus.get(x5 + 1), x4);
 
         cpu.registers.set_bc(x5 + 1);
-        cpu.load(0x0, 0xa);
+        cpu.load((0x0, 0xa));
         assert_eq!(cpu.registers.get_a(), x4);
 
         cpu.bus.set(cpu.registers.get_pc() + 1, x1);
-        cpu.load(1, 0xe);
+        cpu.load((1, 0xe));
         assert_eq!(cpu.registers.get_e(), x1);
     }
 
@@ -201,82 +312,105 @@ mod tests {
         cpu.registers.set_hl(y1);
         cpu.bus.set(y1, x3);
 
-        cpu.alu(0x8, 0x2);
+        cpu.alu(((0x8, 0x2)));
         assert_eq!(cpu.registers.get_a(), x1.wrapping_add(x2));
 
         cpu.registers.set_a(x1);
-        cpu.alu(0x8, 0x6);
+        cpu.alu((0x8, 0x6));
         assert_eq!(cpu.registers.get_a(), x1.wrapping_add(x3));
 
         cpu.registers.set_a(x1);
-        cpu.alu(0x9, 0x2);
+        cpu.alu((0x9, 0x2));
         assert_eq!(cpu.registers.get_a(), x1.wrapping_sub(x2));
 
         cpu.registers.set_a(x1);
-        cpu.alu(0x9, 0x6);
+        cpu.alu((0x9, 0x6));
         assert_eq!(cpu.registers.get_a(), x1.wrapping_sub(x3));
 
         cpu.registers.set_a(x1);
         cpu.registers.set_flag_c(false);
-        cpu.alu(0x8, 0xa);
+        cpu.alu((0x8, 0xa));
         assert_eq!(cpu.registers.get_a(), x1.wrapping_add(x2));
 
         cpu.registers.set_a(x1);
         cpu.registers.set_flag_c(true);
-        cpu.alu(0x8, 0xe);
+        cpu.alu((0x8, 0xe));
         assert_eq!(cpu.registers.get_a(), x1.wrapping_add(x3 + 1));
 
         cpu.registers.set_a(x1);
         cpu.registers.set_flag_c(false);
-        cpu.alu(0x9, 0xa);
+        cpu.alu((0x9, 0xa));
         assert_eq!(cpu.registers.get_a(), x1.wrapping_sub(x2));
 
         cpu.registers.set_a(x1);
         cpu.registers.set_flag_c(true);
-        cpu.alu(0x9, 0xe);
+        cpu.alu((0x9, 0xe));
         assert_eq!(cpu.registers.get_a(), x1.wrapping_sub(x3 + 1));
 
         cpu.registers.set_a(x1);
-        cpu.alu(0xa, 0x2);
+        cpu.alu((0xa, 0x2));
         assert_eq!(cpu.registers.get_a(), x1 & x2);
 
         cpu.registers.set_a(x1);
-        cpu.alu(0xa, 0x6);
+        cpu.alu((0xa, 0x6));
         assert_eq!(cpu.registers.get_a(), x1 & x3);
 
         cpu.registers.set_a(x1);
-        cpu.alu(0xb, 0x2);
+        cpu.alu((0xb, 0x2));
         assert_eq!(cpu.registers.get_a(), x1 | x2);
 
         cpu.registers.set_a(x1);
-        cpu.alu(0xb, 0x6);
+        cpu.alu((0xb, 0x6));
         assert_eq!(cpu.registers.get_a(), x1 | x3);
 
         cpu.registers.set_a(x1);
-        cpu.alu(0xa, 0xa);
+        cpu.alu((0xa, 0xa));
         assert_eq!(cpu.registers.get_a(), x1 ^ x2);
 
         cpu.registers.set_a(x1);
-        cpu.alu(0xa, 0xe);
+        cpu.alu((0xa, 0xe));
         assert_eq!(cpu.registers.get_a(), x1 ^ x3);
 
         cpu.registers.set_a(x4);
         cpu.registers.set_b(x5);
-        cpu.alu(0x9, 0);
+        cpu.alu((0x9, 0));
         assert_eq!(cpu.registers.get_flag_c(), true);
         assert_eq!(cpu.registers.get_flag_n(), true);
         assert_eq!(cpu.registers.get_flag_z(), x4 == x5);
 
         cpu.registers.set_a(x4);
         cpu.registers.set_b(x4);
-        cpu.alu(0xb, 0x8);
+        cpu.alu((0xb, 0x8));
         assert_eq!(cpu.registers.get_flag_n(), true);
         assert_eq!(cpu.registers.get_flag_z(), true);
 
         cpu.registers.set_a(s2);
         cpu.registers.set_b(s2);
-        cpu.alu(0x8, 0);
+        cpu.alu((0x8, 0));
         assert_eq!(cpu.registers.get_flag_n(), false);
         assert_eq!(cpu.registers.get_flag_h(), true);
+    }
+
+    #[test]
+    fn stack() {
+        let mut cpu = Cpu::new();
+        let mut rng = rand::thread_rng();
+
+        let x1 = rng.gen_range(0..=255);
+        let x2 = rng.gen_range(0..=255);
+        let y1 = rng.gen_range(0..=0xFFFF);
+        let y2 = rng.gen_range(0..=0xFFFF);
+
+        cpu.push(y1);
+        assert_eq!(cpu.pop(), y1);
+
+        cpu.push(y1);
+        cpu.push(x1);
+        cpu.push(y2);
+        cpu.push(x2);
+        assert_eq!(cpu.pop(), x2);
+        assert_eq!(cpu.pop(), y2);
+        assert_eq!(cpu.pop(), x1);
+        assert_eq!(cpu.pop(), y1);
     }
 }

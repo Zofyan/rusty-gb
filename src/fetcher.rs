25933743888e84c03bb1d8593ebe971ec76d2da6
Sofyan Aarrass <s.aarrass@secura.com>
2024-05-08 15:59:57 +0200

    fetcher maybe done

--- src/fetcher.rs
+++ src/fetcher.rs
@@ -5,12 +5,11 @@ enum FetcherState {
     ReadTileData0,
     ReadTileData1,
     PushToFIFO,
-    ReadTileID
 }
 
 pub struct Fetcher {
     ticks: usize,
-    tile_number: u8,
+    tile_id: u16,
     x_position_counter: u8,
     pixel_data: [u8; 8],
     state: FetcherState
@@ -20,11 +19,17 @@ impl Fetcher  {
     pub(crate) fn new(bus: &Bus) ->Fetcher {
         Fetcher {
             ticks: 0,
-            tile_number: 0,
+            tile_id: 0,
             x_position_counter: 0,
             pixel_data: [0; 8],
             state: FetcherState::FetchTileNumber,
         }
+    }
+    pub fn reset(&mut self){
+        self.ticks = 0;
+        self.x_position_counter = 0;
+        self.pixel_data.fill(0);
+        self.state = FetcherState::FetchTileNumber
     }
     pub fn tick(&mut self, bus: &mut Bus) {
         self.ticks += 1;
@@ -35,19 +40,48 @@ impl Fetcher  {
             FetcherState::ReadTileData0 => self.read_tile_data0(bus),
             FetcherState::ReadTileData1 => self.read_tile_data1(bus),
             FetcherState::PushToFIFO => self.push_to_fifo(bus),
-            FetcherState::ReadTileID => { }
         }
     }
 
     fn fetch_tile_number(&mut self, bus: &Bus){
-        self.tile_number = 0;
+        self.tile_id = match (bus.get_ldlc_window_enable(), bus.get_ly() >= bus.get_wy(), self.x_position_counter >= bus.get_wx()) {
+            (true, true, true) => {
+                self.x_position_counter as u16 + 32 * (((bus.get_ly() - bus.get_wy()) & 0xFF) as u16 / 8)
+            },
+            (_, _, _) => {
+                self.x_position_counter as u16 + ((bus.get_scx() as u16 / 8) & 0x1f) + 32 * (((bus.get_ly() + bus.get_scy()) & 0xFF) as u16 / 8)
+            },
+        };
         self.pixel_data.fill(0);
         self.state = FetcherState::ReadTileData0
     }
     fn read_tile_data0(&mut self, bus: &Bus){
+        let offset = match bus.get_ldlc_bg_window_tiles() {
+            true => 0x8000 + 16 * self.tile_id,
+            false => 0x9000u16.wrapping_add_signed(self.tile_id as i16 * 16)
+        };
+        let data = bus.get(offset + (bus.get_ly() as u16 % 8) * 2);
+        for bit in 0..=7 {
+            self.pixel_data[bit] = (data >> bit) & 1
+        }
+
     }
     fn read_tile_data1(&mut self, bus: &Bus){
+        let offset = match bus.get_ldlc_bg_window_tiles() {
+            true => 0x8000 + 16 * self.tile_id,
+            false => 0x9000u16.wrapping_add_signed(self.tile_id as i16 * 16)
+        };
+        let data = bus.get(offset + (bus.get_ly() as u16 % 8) * 2 +1);
+        for bit in 0..=7 {
+            self.pixel_data[bit] |= ((data >> bit) & 1) << 1
+        }
     }
     fn push_to_fifo(&mut self, bus: &mut Bus){
+        if bus.fifo.len() <= 8 {
+            for i in (0..=7).rev() {
+                bus.fifo.push(self.pixel_data[i]);
+            }
+
+        }
     }
 }
